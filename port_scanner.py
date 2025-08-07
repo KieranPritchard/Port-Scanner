dc1764ac260341f53e6459dba467ba5c79e640d7
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-08-07 22:03:16 +0100

    added scan most ports functions

--- port_scanner.py
+++ port_scanner.py
@@ -14,6 +14,20 @@ def scan_all_ports(target):
             print(f"Port {port} is CLOSED")
         sock.close()
 
+def scan_most_used(target):
+    port_list = [20, 21, 22, 23, 25, 53, 80, 110, 119, 123, 143, 161, 194, 443]
+
+    print(f"Scanning {target}")
+    for port in port_list:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.timeout(0.5)
+        result = sock.connect_ex((target, port))
+        if result == 0:
+            print(f"Port {port} is OPEN")
+        else:
+            print(f"Port {port} is CLOSED")
+        sock.close()
+
 def main():
     #banner for start up of program
     print("=" * 30)
@@ -28,8 +42,9 @@ def main():
     choice = int(input("Please select your option: "))
 
     if choice == 1:
-        # function input logic here
-        print("")
+        # Scans most used ports on the ip address
+        target = input("Please input your target ip address:")
+        scan_most_used(target)
     elif choice == 2:
         # Scans all the ports on the ip address
         target = input("Please input your target ip address:")
